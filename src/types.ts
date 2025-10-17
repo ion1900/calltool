9b30209053ed2ba7dc63a64e9a59395bbf1215ee
chu2bard <bxndnbdnd29@gmail.com>
2025-10-17 15:47:53 +0200

    add openai dependency

---
+++ src/types.ts
@@ -0,0 +1,32 @@
+import { z } from "zod";
+
+export interface ToolDefinition {
+  name: string;
+  description: string;
+  parameters: Record<string, unknown>;
+  handler: (...args: any[]) => any | Promise<any>;
+}
+
+export interface ToolCall {
+  name: string;
+  arguments: Record<string, unknown>;
+}
+
+export interface ToolResult {
+  name: string;
+  result: unknown;
+  error?: string;
+}
+
+export interface FunctionSchema {
+  type: "function";
+  function: {
+    name: string;
+    description: string;
+    parameters: {
+      type: "object";
+      properties: Record<string, unknown>;
+      required: string[];
+    };
+  };
+}
