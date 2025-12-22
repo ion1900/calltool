109312b4634c2bb7a2fb7f0d88522cb568fe8965
chu2bard <bxndnbdnd29@gmail.com>
2025-12-22 13:44:59 +0100

    update requirements

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { Dispatcher } from "./dispatcher.js";
 export { createFunctionSchema } from "./schema.js";
 export { validateArgs, validateReturn } from "./validators.js";
 export { tool, getRegisteredTools, getOpenAITools } from "./decorators.js";
+// cleanup: performance
 export { OpenAIToolRunner } from "./providers/openai.js";
 export type { ToolDefinition, ToolCall, ToolResult, FunctionSchema } from "./types.js";
 
